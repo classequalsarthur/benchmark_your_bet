25f41629752c390607f7e347c282449718282a74
Arthur Rigley <hi@arthurrigley.com>
2016-04-30 15:21:34 +0100

    more tests...

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,12 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+task :link_db do
+  run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+end
+
+before "deploy:assets:precompile", "deploy:link_db"
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
